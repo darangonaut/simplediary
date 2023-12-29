d91742f353faa5682dbccd529c272a5d1806b520
Markuška Peter <peter.markuska@dek-cz.com>
2023-12-29 17:18:56 +0100

    Update text color and date format in post index view

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -72,12 +72,12 @@
             @foreach ($posts as $post)
             <div class="flex flex-col justify-between bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-4 dark:text-gray-200 ">
                 
-                <div class="text-gray-400 mr-4 mb-2">
+                <div class="text-gray-600 dark:text-gray-200 mr-4 mb-2">
                     {!! nl2br(e($post->body)) !!}
                 </div>
 
                 <div class="flex flex-row h-8 items-baseline">
-                    <span class="w-full text-gray-400 text-sm mr-4">
+                    <span class="w-full text-gray-400 dark:text-gray-400 text-sm mr-4">
                         {{ $post->updated_at->format('Y-m-d') }}
                     </span>
 
